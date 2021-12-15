2703faa9f5ace2c2f6dd742df676e2cc7304c5e1
KurtReyn <kurtreyn@yahoo.com>
2021-12-15 13:27:46 -0500

    week 2 workshop

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from 'reactstrap';
+
+class CampsiteInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsite: null,
+    };
+  }
+
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  //   renderComments(comments) {
+  //     if (comments) {
+  //       return (
+  //         <div className="col-md-5 m-1">
+  //           <h4>Comments</h4>
+  //           {comments.map((comment) => (
+  //             <div key={comment.id}>
+  //               {comment.text}
+  //               {comment.author}
+  //               {new Intl.DateTimeFormat('en-US', {
+  //                 year: 'numeric',
+  //                 month: 'short',
+  //                 day: '2-digit',
+  //               }).format(new Date(Date.parse(comment.date)))}{' '}
+  //             </div>
+  //           ))}
+  //         </div>
+  //       );
+  //     }
+  //     return <div />;
+  //   }
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => {
+            <div key={comment.id}></div>;
+            <p>{comment.text}</p>;
+            <p>{comment.author}</p>;
+            {
+              new Intl.DateTimeFormat('en-US', {
+                year: 'numeric',
+                month: 'short',
+                day: '2-digit',
+              }).format(new Date(Date.parse(comment.date)));
+            }
+          })}
+        </div>
+      );
+    }
+    return <div></div>;
+  }
+
+  render() {
+    if (this.props.campsite) {
+      return (
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.comments)}
+        </div>
+      );
+    }
+
+    return <div></div>;
+  }
+}
+
+export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CampsiteInfo from './CampsiteInfoComponent';
 import {
   Card,
   CardImg,
@@ -20,20 +21,20 @@ class Directory extends Component {
     this.setState({ selectedCampsite: campsite });
   }
 
-  renderSelectedCampsite(campsite) {
-    if (campsite) {
-      return (
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
-    return <div></div>;
-  }
+  // renderSelectedCampsite(campsite) {
+  //   if (campsite) {
+  //     return (
+  //       <Card>
+  //         <CardImg top src={campsite.image} alt={campsite.name} />
+  //         <CardBody>
+  //           <CardTitle>{campsite.name}</CardTitle>
+  //           <CardText>{campsite.description}</CardText>
+  //         </CardBody>
+  //       </Card>
+  //     );
+  //   }
+  //   return <div></div>;
+  // }
 
   render() {
     const directory = this.props.campsites.map((campsite) => {
@@ -52,11 +53,12 @@ class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <div className="row">
+        {/* <div className="row">
           <div className="col-md-5 m-1">
             {this.renderSelectedCampsite(this.state.selectedCampsite)}
           </div>
-        </div>
+        </div> */}
+        <CampsiteInfo campsite={this.state.selectedCampsite}></CampsiteInfo>
       </div>
     );
   }
