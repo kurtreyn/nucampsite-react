15682e36b78a1308d2cf5b637a1ddf073e6af337
KurtReyn <kurtreyn@yahoo.com>
2021-12-21 09:22:33 -0500

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 function RenderCampsite({ campsite }) {
   return (
